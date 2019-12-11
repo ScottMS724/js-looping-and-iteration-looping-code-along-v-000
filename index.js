072918b91d86384b9494813e57ac6ee4694aa8ab
Scott Silverman <scottms724@gmail.com>
2019-12-11 17:29:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-function writeCards(array, event)
+function writeCards(array, event) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(`Thank you, ${array[i]}, for the wonderful birthday gift!`)
+  }
+}
