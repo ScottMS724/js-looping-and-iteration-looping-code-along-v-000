af6f1d4573110ff5b2341c36481a3892a0aff490
Scott Silverman <scottms724@gmail.com>
2019-12-11 17:32:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+array = ["Lisa", "Kaitlin", "Jan"]
+
 function writeCards(array) {
   for (let i = 0; i < array.length; i++) {
     console.log(`Thank you, ${array[i]}, for the wonderful birthday gift!`);
